66a2c684ad5797dc97734e96a7b824d8c9d0e3de
aso <titechei@gmail.com>
2019-06-07 18:45:26 +0900

    receive broadcast

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,19 +1,39 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    private BroadcastReceiver scanReceiver;
+    private IntentFilter scanFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+
+        scanReceiver = new BroadcastReceiver(){
+            private static final String TAG = "BroadcastReceiver";
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, intent.getStringExtra(Service3.EXTRA_ANSWER));
+                Toast.makeText(context, intent.getStringExtra(Service3.EXTRA_ANSWER), Toast.LENGTH_LONG).show();
+            }
+        };
+
+        scanFilter = new IntentFilter();
+        scanFilter.addAction(Service3.ACTION_ANSWER);
+
+        registerReceiver(scanReceiver, scanFilter);
     }
 
     public void onClickTest1(View v) {
@@ -27,6 +47,13 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onClickTest2 in " + Thread.currentThread());
         Intent intent = new Intent(this, Service2.class);
         intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+
+    public void onClickTest3(View v) {
+        Log.d(TAG, "onClickTest3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, Service3.class);
+        intent.putExtra(Service3.EXTRA_MYARG, "Hello, Service3");
         startService(intent);
     }
 }
